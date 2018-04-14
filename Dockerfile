fda389ff5668697e5a8bd723c195b48a8e02a0af
maeda <duck8823@gmail.com>
2018-04-14 23:22:32 +0900

    Add docker files

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:alpine
+MAINTAINER shunsuke maeda <duck8823@gmail.com>
+
+RUN apk --update add --no-cache git
+
+WORKDIR /go/src/github.com/duck8823/webhook-proxy
+
+ADD . .
+
+RUN go get -u github.com/golang/dep/cmd/dep && \
+    dep ensure
+
+RUN go build
+
+EXPOSE 8080
+
+CMD ["./webhook-proxy"]
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/duck8823/webhook-proxy/payloads"
+	"github.com/duck8823/webhook-proxy/proxy/handlers"
+	"github.com/google/logger"
+	"net/http"
+	"os"
+)
+
+func main() {
+	logger.Init("webhook-proxy", false, false, os.Stdout)
+
+	http.Handle("/", &handlers.SlackNotificator{
+		Url: "https://hooks.slack.com/services/XXXX/YYYY/ZZZZ",
+		ConvertFunc: func(body []byte) (*payloads.SlackMessage, error) {
+			commitComment := &payloads.GitHubCommitComment{}
+			if err := json.Unmarshal(body, commitComment); err != nil {
+				return nil, err
+			}
+
+			return &payloads.SlackMessage{
+				Text: fmt.Sprintf("repository: %s", commitComment.Repository.FullName),
+			}, nil
+		},
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
