072c732ab9eeefb6d6af7d81b99953a0656d0d06
shunsuke maeda <duck8823@gmail.com>
2018-07-05 08:37:11 +0900

    Use gomock

--- infrastructure/context/context_test.go
+++ infrastructure/context/context_test.go
@@ -23,7 +23,7 @@ func TestWithTimeout(t *testing.T) {
 		defer cancel()
 
 		go func() {
-			time.Sleep(10*time.Millisecond)
+			time.Sleep(10 * time.Millisecond)
 			cancel()
 		}()
 
--- service/runner/runner_test.go
+++ service/runner/runner_test.go
@@ -1,15 +1,15 @@
 package runner_test
 
 import (
-	"fmt"
 	"github.com/duck8823/minimal-ci/infrastructure/context"
 	"github.com/duck8823/minimal-ci/service/github"
+	"github.com/duck8823/minimal-ci/service/github/mock_github"
 	"github.com/duck8823/minimal-ci/service/runner"
+	"github.com/golang/mock/gomock"
 	goGithub "github.com/google/go-github/github"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"os"
 	"path"
-	"reflect"
 	"testing"
 )
 
@@ -26,48 +26,20 @@ func (r *MockRepo) GetSSHURL() string {
 	return r.SSHURL
 }
 
-type MockGitHub struct {
-}
-
-func (g *MockGitHub) GetPullRequest(ctx context.Context, repository github.Repository, num int) (*github.PullRequest, error) {
-	return &github.PullRequest{
-		Head: &goGithub.PullRequestBranch{
-			Ref: goGithub.String("master"),
-		},
-	}, nil
-}
-
-func (g *MockGitHub) CreateCommitStatus(ctx context.Context, repository github.Repository, hash plumbing.Hash, status *github.Status) error {
-	expected := plumbing.Hash{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	if !reflect.DeepEqual(expected, hash) {
-		return fmt.Errorf("hash must be equal %+v, but got %+v", expected, hash)
-	}
-	return nil
-}
-
-func (g *MockGitHub) Clone(ctx context.Context, dir string, repo github.Repository, ref string) (plumbing.Hash, error) {
-	if err := os.MkdirAll(dir, 0700); err != nil {
-		return plumbing.Hash{}, err
-	}
-
-	dockerfile, err := os.OpenFile(path.Join(dir, "Dockerfile"), os.O_RDWR|os.O_CREATE, 0600)
-	if err != nil {
-		return plumbing.Hash{}, err
-	}
-	defer dockerfile.Close()
-
-	dockerfile.WriteString("FROM alpine\nENTRYPOINT [\"echo\"]")
-
-	return plumbing.Hash{1, 2, 3, 4, 5, 6, 7, 8, 9}, nil
-}
-
 func TestRunnerImpl_ConvertPullRequestToRef(t *testing.T) {
 	r, err := runner.NewWithEnv()
 	if err != nil {
 		t.Fatalf("error occured. %+v", err)
 	}
 
-	r.GitHub = &MockGitHub{}
+	ctrl := gomock.NewController(t)
+	mockGitHub := mock_github.NewMockService(ctrl)
+	mockGitHub.EXPECT().GetPullRequest(gomock.Any(), gomock.Any(), gomock.Any()).
+		Times(1).
+		Return(&github.PullRequest{
+			Head: &goGithub.PullRequestBranch{Ref: goGithub.String("master")},
+		}, nil)
+	r.GitHub = mockGitHub
 
 	actual, err := r.ConvertPullRequestToRef(context.New(), &MockRepo{}, 5)
 	if err != nil {
@@ -86,7 +58,29 @@ func TestRunnerImpl_Run(t *testing.T) {
 		t.Fatalf("error occured. %+v", err)
 	}
 
-	r.GitHub = &MockGitHub{}
+	ctrl := gomock.NewController(t)
+	mockGitHub := mock_github.NewMockService(ctrl)
+	mockGitHub.EXPECT().CreateCommitStatus(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		Times(1).
+		Return(nil)
+	mockGitHub.EXPECT().Clone(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		Times(1).
+		DoAndReturn(func(ctx context.Context, dir string, repo github.Repository, ref string) (plumbing.Hash, error) {
+			if err := os.MkdirAll(dir, 0700); err != nil {
+				return plumbing.Hash{}, err
+			}
+
+			dockerfile, err := os.OpenFile(path.Join(dir, "Dockerfile"), os.O_RDWR|os.O_CREATE, 0600)
+			if err != nil {
+				return plumbing.Hash{}, err
+			}
+			defer dockerfile.Close()
+
+			dockerfile.WriteString("FROM alpine\nENTRYPOINT [\"echo\"]")
+
+			return plumbing.Hash{1, 2, 3, 4, 5, 6, 7, 8, 9}, nil
+		})
+	r.GitHub = mockGitHub
 
 	repo := &MockRepo{"duck8823/minimal-ci", "git@github.com:duck8823/minimal-ci.git"}
 	hash, err := r.Run(context.New(), repo, "master", "Hello World.")
