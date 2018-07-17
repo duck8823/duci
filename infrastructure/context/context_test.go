7a868372c3b11fb4a37f1fbc044502efa77f7042
shunsuke maeda <duck8823@gmail.com>
2018-07-18 00:21:23 +0900

    Add comment

--- infrastructure/context/context_test.go
+++ infrastructure/context/context_test.go
@@ -9,9 +9,11 @@ import (
 )
 
 func TestContextWithUUID_UUID(t *testing.T) {
+	// given
 	ctx := context.New("test/task")
-
 	var empty uuid.UUID
+
+	// expect
 	if ctx.UUID() == empty {
 		t.Error("UUID() must not empty.")
 	}
@@ -19,6 +21,7 @@ func TestContextWithUUID_UUID(t *testing.T) {
 
 func TestWithTimeout(t *testing.T) {
 	t.Run("when timeout", func(t *testing.T) {
+		// when
 		ctx, cancel := context.WithTimeout(context.New("test/task"), 5*time.Millisecond)
 		defer cancel()
 
@@ -29,12 +32,14 @@ func TestWithTimeout(t *testing.T) {
 
 		<-ctx.Done()
 
+		// then
 		if ctx.Err() != ct.DeadlineExceeded {
 			t.Errorf("not expected error. wont: %+v, but got %+v", ct.DeadlineExceeded, ctx.Err())
 		}
 	})
 
 	t.Run("when cancel", func(t *testing.T) {
+		// when
 		ctx, cancel := context.WithTimeout(context.New("test/task"), 5*time.Millisecond)
 		defer cancel()
 
@@ -44,6 +49,7 @@ func TestWithTimeout(t *testing.T) {
 
 		<-ctx.Done()
 
+		// then
 		if ctx.Err() != ct.Canceled {
 			t.Errorf("not expected error. wont: %+v, but got %+v", ct.Canceled, ctx.Err())
 		}
