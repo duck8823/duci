d194faaf62bde5377e85e040f128ec0faacf61eb
shunsuke maeda <duck8823@gmail.com>
2018-07-22 14:56:52 +0900

    Add test pattern with config file

--- application/service/runner/runner_test.go
+++ application/service/runner/runner_test.go
@@ -156,6 +156,73 @@ func TestRunnerImpl_Run(t *testing.T) {
 		})
 	})
 
+	t.Run("with config file", func(t *testing.T) {
+		// given
+		mockGitHub := mock_github.NewMockService(ctrl)
+		mockGitHub.EXPECT().CreateCommitStatus(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+			Times(2).
+			Return(nil)
+
+		// and
+		mockGit := mock_git.NewMockClient(ctrl)
+		mockGit.EXPECT().Clone(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+			Times(1).
+			DoAndReturn(func(_ context.Context, dir string, _ string, _ string) (plumbing.Hash, error) {
+				if err := os.MkdirAll(path.Join(dir, ".duci"), 0700); err != nil {
+					return plumbing.Hash{}, err
+				}
+
+				dockerfile, err := os.OpenFile(path.Join(dir, ".duci/config.yml"), os.O_RDWR|os.O_CREATE, 0600)
+				if err != nil {
+					return plumbing.Hash{}, err
+				}
+				defer dockerfile.Close()
+
+				dockerfile.WriteString("---\nvolumes:\n  - /hello:/hello")
+
+				return plumbing.Hash{1, 2, 3, 4, 5, 6, 7, 8, 9}, nil
+			})
+
+		// and
+		mockDocker := mock_docker.NewMockClient(ctrl)
+		mockDocker.EXPECT().
+			Build(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+			Return(nil)
+		mockDocker.EXPECT().
+			Run(gomock.Any(), gomock.Eq(docker.RuntimeOptions{Volumes: []string{"/hello:/hello"}}), gomock.Any(), gomock.Any()).
+			Times(1).
+			Return("", nil)
+		mockDocker.EXPECT().
+			Run(gomock.Any(), gomock.Not(docker.RuntimeOptions{Volumes: []string{"/hello:/hello"}}), gomock.Any(), gomock.Any()).
+			Return("", errors.New("must not call this"))
+
+		r := &runner.DockerRunner{
+			Name:        "test-runner",
+			BaseWorkDir: path.Join(os.TempDir(), "test-runner"),
+			Git:         mockGit,
+			GitHub:      mockGitHub,
+			Docker:      mockDocker,
+		}
+
+		// and
+		repo := &MockRepo{"duck8823/duci", "git@github.com:duck8823/duci.git"}
+
+		// and
+		var empty plumbing.Hash
+
+		// when
+		hash, err := r.Run(context.New("test/task"), repo, "master", "Hello World.")
+
+		// then
+		if err != nil {
+			t.Errorf("must not error. but: %+v", err)
+		}
+
+		if hash == empty {
+			t.Error("hash must not empty")
+		}
+	})
+
 	t.Run("when failed to git clone", func(t *testing.T) {
 		// given
 		mockGitHub := mock_github.NewMockService(ctrl)
