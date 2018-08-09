c4a6bfcf7c8635880bb49a8509701f32eac9a9ef
shunsuke maeda <duck8823@gmail.com>
2018-08-10 08:35:32 +0900

    Fix docker logger

--- infrastructure/docker/logger.go
+++ infrastructure/docker/logger.go
@@ -24,12 +24,7 @@ type buildLogger struct {
 
 func (l *buildLogger) ReadLine() (*LogLine, error) {
 	line, _, err := l.reader.ReadLine()
-	return &LogLine{Timestamp: clock.Now(), Message: line}, nil
-	if err == io.EOF {
-		return nil, io.EOF
-	} else if err != nil {
-		return nil, errors.WithStack(err)
-	}
+	return &LogLine{Timestamp: clock.Now(), Message: line}, err
 }
 
 type runLogger struct {
@@ -39,22 +34,21 @@ type runLogger struct {
 func (l *runLogger) ReadLine() (*LogLine, error) {
 	for {
 		line, _, err := l.reader.ReadLine()
-		if len(line) > 8 {
-			// detect log prefix
-			// see https://godoc.org/github.com/docker/docker/client#Client.ContainerLogs
-			if !((line[0] == 1 || line[0] == 2) && (line[1] == 0 && line[2] == 0 && line[3] == 0)) {
-				continue
-			}
-			messages := line[8:]
-
-			// prevent to CR
-			progress := bytes.Split(messages, []byte{'\r'})
-			return &LogLine{Timestamp: clock.Now(), Message: progress[0]}, nil
-		}
-		if err == io.EOF {
-			return nil, io.EOF
-		} else if err != nil {
+		if err != nil && err != io.EOF {
 			return nil, errors.WithStack(err)
+		} else if len(line) < 8 {
+			return nil, errors.New("line too short")
 		}
+
+		// detect log prefix
+		// see https://godoc.org/github.com/docker/docker/client#Client.ContainerLogs
+		if !((line[0] == 1 || line[0] == 2) && (line[1] == 0 && line[2] == 0 && line[3] == 0)) {
+			continue
+		}
+		messages := line[8:]
+
+		// prevent to CR
+		progress := bytes.Split(messages, []byte{'\r'})
+		return &LogLine{Timestamp: clock.Now(), Message: progress[0]}, err
 	}
 }
