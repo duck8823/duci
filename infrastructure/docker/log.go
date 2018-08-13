b6ad4fd86d3e0f7f55d4503037183927b29b74b9
shunsuke maeda <duck8823@gmail.com>
2018-08-14 00:57:10 +0900

    Separate function

--- infrastructure/docker/log.go
+++ infrastructure/docker/log.go
@@ -36,19 +36,28 @@ func (l *runLogger) ReadLine() (*LogLine, error) {
 		line, _, err := l.reader.ReadLine()
 		if err != nil && err != io.EOF {
 			return nil, errors.WithStack(err)
-		} else if len(line) < 8 {
-			return nil, errors.New("line too short")
 		}
 
-		// detect log prefix
-		// see https://godoc.org/github.com/docker/docker/client#Client.ContainerLogs
-		if !((line[0] == 1 || line[0] == 2) && (line[1] == 0 && line[2] == 0 && line[3] == 0)) {
-			continue
+		messages, err := trimPrefix(line)
+		if err != nil {
+			return nil, errors.WithStack(err)
 		}
-		messages := line[8:]
 
 		// prevent to CR
 		progress := bytes.Split(messages, []byte{'\r'})
 		return &LogLine{Timestamp: clock.Now(), Message: progress[0]}, err
 	}
+}
+
+func trimPrefix(line []byte) ([]byte, error) {
+	if len(line) < 8 {
+		return nil, errors.New("line too short")
+	}
+
+	// detect log prefix
+	// see https://godoc.org/github.com/docker/docker/client#Client.ContainerLogs
+	if !((line[0] == 1 || line[0] == 2) && (line[1] == 0 && line[2] == 0 && line[3] == 0)) {
+		return nil, errors.New("invalid log prefix")
+	}
+	return line[8:], nil
 }
