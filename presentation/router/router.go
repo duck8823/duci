e1f2e1ad8ec3848e5809e2e3937a60dae49b3b0e
shunsuke maeda <duck8823@gmail.com>
2018-08-23 21:13:48 +0900

    Separate function

--- presentation/router/router.go
+++ presentation/router/router.go
@@ -14,15 +14,7 @@ import (
 )
 
 func New() (http.Handler, error) {
-	gitClient, err := git.New(application.Config.Server.SSHKeyPath)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
 	githubService, err := github.NewWithEnv()
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	dockerClient, err := docker.New()
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
@@ -30,14 +22,9 @@ func New() (http.Handler, error) {
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
-
-	dockerRunner := &runner.DockerRunner{
-		Name:        application.Name,
-		BaseWorkDir: application.Config.Server.WorkDir,
-		Git:         gitClient,
-		GitHub:      githubService,
-		Docker:      dockerClient,
-		LogStore:    logStore,
+	dockerRunner, err := createRunner(logStore, githubService)
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
 
 	webhooksCtrl := &controller.JobController{Runner: dockerRunner, GitHub: githubService}
@@ -48,4 +35,26 @@ func New() (http.Handler, error) {
 	rtr.Get("/logs/{uuid}", logCtrl.ServeHTTP)
 
 	return rtr, nil
+}
+
+func createRunner(logStore log.StoreService, github github.Service) (runner.Runner, error) {
+	gitClient, err := git.New(application.Config.Server.SSHKeyPath)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	dockerClient, err := docker.New()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	dockerRunner := &runner.DockerRunner{
+		Name:        application.Name,
+		BaseWorkDir: application.Config.Server.WorkDir,
+		Git:         gitClient,
+		GitHub:      github,
+		Docker:      dockerClient,
+		LogStore:    logStore,
+	}
+
+	return dockerRunner, nil
 }
