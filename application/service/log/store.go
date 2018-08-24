69c3fcb5d5c907b07404d0e1f07ff27253013685
shunsuke maeda <duck8823@gmail.com>
2018-08-24 10:40:02 +0900

    Delete unnecessary code

--- application/service/log/store.go
+++ application/service/log/store.go
@@ -7,6 +7,8 @@ import (
 	"github.com/duck8823/duci/infrastructure/logger"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/storage"
 )
 
 type Level = string
@@ -23,7 +25,7 @@ type storeServiceImpl struct {
 }
 
 func NewStoreService() (StoreService, error) {
-	database, err := logger.OpenMemDb()
+	database, err := leveldb.Open(storage.NewMemStorage(), nil)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
