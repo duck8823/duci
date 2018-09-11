1af9cfc674dada1679461a9b5882b10722c876f1
shunsuke maeda <duck8823@gmail.com>
2018-09-12 08:05:45 +0900

    Separate methods

--- presentation/controller/webhooks.go
+++ presentation/controller/webhooks.go
@@ -26,67 +26,20 @@ type WebhooksController struct {
 }
 
 func (c *WebhooksController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	deliveryID := go_github.DeliveryID(r)
-	requestID, err := uuid.Parse(deliveryID)
+	requestID, err := requestID(r)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: invalid request header `X-GitHub-Delivery`: %+v", deliveryID), http.StatusBadRequest)
+		logger.Error(requestID, err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
-	}
-
-	runtimeURL := &url.URL{
-		Scheme: "http",
-		Host:   r.Host,
-		Path:   r.URL.Path,
-	}
-	if r.URL.Scheme != "" {
-		runtimeURL.Scheme = r.URL.Scheme
 	}
 
 	// Trigger build
 	githubEvent := r.Header.Get("X-GitHub-Event")
 	switch githubEvent {
 	case "issue_comment":
-		// Read Payload
-		event := &go_github.IssueCommentEvent{}
-		if err := json.NewDecoder(r.Body).Decode(event); err != nil {
-			logger.Errorf(requestID, "%+v", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		ctx, repo, head, command, err := c.parseIssueComment(event, requestID, runtimeURL)
-		if err == SkipBuild {
-			logger.Info(requestID, "skip build")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(err.Error()))
-			return
-		} else if err != nil {
-			logger.Errorf(requestID, "%+v", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		ref := fmt.Sprintf("refs/heads/%s", head.GetRef())
-		go c.Runner.Run(ctx, repo, ref, plumbing.NewHash(head.GetSHA()), command...)
+		c.runWithIssueCommentEvent(requestID, w, r)
 	case "push":
-		event := &go_github.PushEvent{}
-		if err := json.NewDecoder(r.Body).Decode(event); err != nil {
-			logger.Errorf(requestID, "%+v", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		sha := event.GetHeadCommit().GetID()
-		if len(sha) == 0 {
-			logger.Info(requestID, "skip build: could not get head commit")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("skip build"))
-			return
-		}
-
-		taskName := fmt.Sprintf("%s/push", application.Name)
-		ctx := context.New(taskName, requestID, runtimeURL)
-		go c.Runner.Run(ctx, event.GetRepo(), event.GetRef(), plumbing.NewHash(sha))
+		c.runWithPushEvent(requestID, w, r)
 	default:
 		message := fmt.Sprintf("payload event type must be issue_comment or push. but %s", githubEvent)
 		logger.Error(requestID, message)
@@ -98,20 +51,44 @@ func (c *WebhooksController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func (c *WebhooksController) runWithIssueCommentEvent(
+	requestID uuid.UUID,
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	// Read Payload
+	event := &go_github.IssueCommentEvent{}
+	if err := json.NewDecoder(r.Body).Decode(event); err != nil {
+		logger.Errorf(requestID, "%+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ctx, repo, head, command, err := c.parseIssueComment(event, requestID, runtimeURL(r))
+	if err == SkipBuild {
+		logger.Info(requestID, "skip build")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(err.Error()))
+		return
+	} else if err != nil {
+		logger.Errorf(requestID, "%+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ref := fmt.Sprintf("refs/heads/%s", head.GetRef())
+	go c.Runner.Run(ctx, repo, ref, plumbing.NewHash(head.GetSHA()), command...)
+}
+
 func (c *WebhooksController) parseIssueComment(
 	event *go_github.IssueCommentEvent,
 	requestID uuid.UUID,
 	url *url.URL,
 ) (ctx context.Context, repo *go_github.Repository, head *go_github.PullRequestBranch, command []string, err error) {
-
-	if !isValidAction(event.Action) {
+	phrase, err := phrase(event)
+	if err != nil {
 		return nil, nil, nil, nil, SkipBuild
 	}
-
-	if !regexp.MustCompile("^ci\\s+[^\\s]+").Match([]byte(event.Comment.GetBody())) {
-		return nil, nil, nil, nil, SkipBuild
-	}
-	phrase := regexp.MustCompile("^ci\\s+").ReplaceAllString(event.Comment.GetBody(), "")
 	command = strings.Split(phrase, " ")
 	ctx = context.New(fmt.Sprintf("%s/pr/%s", application.Name, command[0]), requestID, url)
 
@@ -123,6 +100,61 @@ func (c *WebhooksController) parseIssueComment(
 	repo = event.GetRepo()
 	head = pr.GetHead()
 	return ctx, repo, head, command, err
+}
+
+func (c *WebhooksController) runWithPushEvent(requestID uuid.UUID, w http.ResponseWriter, r *http.Request) {
+	event := &go_github.PushEvent{}
+	if err := json.NewDecoder(r.Body).Decode(event); err != nil {
+		logger.Errorf(requestID, "%+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	sha := event.GetHeadCommit().GetID()
+	if len(sha) == 0 {
+		logger.Info(requestID, "skip build: could not get head commit")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("skip build"))
+		return
+	}
+
+	taskName := fmt.Sprintf("%s/push", application.Name)
+	ctx := context.New(taskName, requestID, runtimeURL(r))
+	go c.Runner.Run(ctx, event.GetRepo(), event.GetRef(), plumbing.NewHash(sha))
+}
+
+func requestID(r *http.Request) (uuid.UUID, error) {
+	deliveryID := go_github.DeliveryID(r)
+	requestID, err := uuid.Parse(deliveryID)
+	if err != nil {
+		msg := fmt.Sprintf("Error: invalid request header `X-GitHub-Delivery`: %+v", deliveryID)
+		return uuid.New(), errors.Wrap(err, msg)
+	}
+	return requestID, nil
+}
+
+func runtimeURL(r *http.Request) *url.URL {
+	runtimeURL := &url.URL{
+		Scheme: "http",
+		Host:   r.Host,
+		Path:   r.URL.Path,
+	}
+	if r.URL.Scheme != "" {
+		runtimeURL.Scheme = r.URL.Scheme
+	}
+	return runtimeURL
+}
+
+func phrase(event *go_github.IssueCommentEvent) (string, error) {
+	if !isValidAction(event.Action) {
+		return "", SkipBuild
+	}
+
+	if !regexp.MustCompile("^ci\\s+[^\\s]+").Match([]byte(event.Comment.GetBody())) {
+		return "", SkipBuild
+	}
+	phrase := regexp.MustCompile("^ci\\s+").ReplaceAllString(event.Comment.GetBody(), "")
+	return phrase, nil
 }
 
 func isValidAction(action *string) bool {
