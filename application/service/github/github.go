55bdad3075c6d433cfa74866d7a3e3e86aa91f07
shunsuke maeda <duck8823@gmail.com>
2018-09-17 15:14:36 +0900

    Add document

--- application/service/github/github.go
+++ application/service/github/github.go
@@ -14,9 +14,13 @@ import (
 type State = string
 
 const (
+	// PENDING represents pending state.
 	PENDING State = "pending"
+	// SUCCESS represents success state.
 	SUCCESS State = "success"
+	// ERROR represents error state.
 	ERROR   State = "error"
+	// FAILURE represents failure state.
 	FAILURE State = "failure"
 )
 
