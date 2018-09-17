ca7e866551df8849e9741081aa33d8e70e93a16e
shunsuke maeda <duck8823@gmail.com>
2018-09-17 15:42:37 +0900

    Use `fmt.Errorf` instead of `errors.New(fmt.Sprintf`

--- application/service/github/repository_name.go
+++ application/service/github/repository_name.go
@@ -2,7 +2,6 @@ package github
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"strings"
 )
 
@@ -15,7 +14,7 @@ type RepositoryName struct {
 func (r *RepositoryName) Owner() (string, error) {
 	ss := strings.Split(r.FullName, "/")
 	if len(ss) != 2 {
-		return "", errors.New(fmt.Sprintf("Invalid repository name: %s", r.FullName))
+		return "", fmt.Errorf("Invalid repository name: %s ", r.FullName)
 	}
 	return ss[0], nil
 }
@@ -24,7 +23,7 @@ func (r *RepositoryName) Owner() (string, error) {
 func (r *RepositoryName) Repo() (string, error) {
 	ss := strings.Split(r.FullName, "/")
 	if len(ss) != 2 {
-		return "", errors.New(fmt.Sprintf("Invalid repository name: %s", r.FullName))
+		return "", fmt.Errorf("Invalid repository name: %s ", r.FullName)
 	}
 	return ss[1], nil
 }
