818f9a47272c4b8e3f5d7d0db9004f6a3daad56f
shunsuke maeda <duck8823@gmail.com>
2018-10-20 07:42:37 +0900

    Use cobra

--- main.go
+++ main.go
@@ -7,32 +7,41 @@ import (
 	"github.com/duck8823/duci/infrastructure/logger"
 	"github.com/duck8823/duci/presentation/router"
 	"github.com/google/uuid"
+	"github.com/spf13/cobra"
 	"net/http"
 	"os"
 )
 
 func main() {
-	mainID := uuid.New()
+	rootCmd := &cobra.Command{Use: "duci"}
+	rootCmd.AddCommand(&cobra.Command{
+		Use: "server",
+		Run: func(cmd *cobra.Command, args []string) {
+			mainID := uuid.New()
 
-	flag.Var(application.Config, "c", "configuration file path")
-	flag.Parse()
+			flag.Var(application.Config, "c", "configuration file path")
+			flag.Parse()
 
-	if err := semaphore.Make(); err != nil {
-		logger.Errorf(mainID, "Failed to initialize a semaphore.\n%+v", err)
-		os.Exit(1)
-		return
-	}
+			if err := semaphore.Make(); err != nil {
+				logger.Errorf(mainID, "Failed to initialize a semaphore.\n%+v", err)
+				os.Exit(1)
+				return
+			}
 
-	rtr, err := router.New()
-	if err != nil {
-		logger.Errorf(mainID, "Failed to initialize controllers.\n%+v", err)
-		os.Exit(1)
-		return
-	}
+			rtr, err := router.New()
+			if err != nil {
+				logger.Errorf(mainID, "Failed to initialize controllers.\n%+v", err)
+				os.Exit(1)
+				return
+			}
 
-	if err := http.ListenAndServe(application.Config.Addr(), rtr); err != nil {
-		logger.Errorf(mainID, "Failed to run server.\n%+v", err)
-		os.Exit(1)
-		return
-	}
+			if err := http.ListenAndServe(application.Config.Addr(), rtr); err != nil {
+				logger.Errorf(mainID, "Failed to run server.\n%+v", err)
+				os.Exit(1)
+				return
+			}
+		},
+	})
+
+	rootCmd.Execute()
 }
