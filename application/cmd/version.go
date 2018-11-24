4993d9d4e066429bfff1a43e3b29999da313fdf8
shunsuke maeda <duck8823@gmail.com>
2018-11-24 18:40:41 +0900

    Separate concerns about version

--- application/cmd/version.go
+++ application/cmd/version.go
@@ -2,26 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/duck8823/duci/application"
 	"github.com/spf13/cobra"
-	"github.com/tcnksm/go-latest"
 	"os"
 )
 
-var (
-	version  = "dev"
-	revision = "unknown"
-)
 var versionCmd = createCmd("version", "Display version", displayVersion)
 
 func displayVersion(cmd *cobra.Command, _ []string) {
 	readConfiguration(cmd)
 
-	fmt.Printf("Version: %s (%s)\n", version, revision)
-	res, _ := latest.Check(&latest.GithubTag{Owner: "duck8823", Repository: "duci"}, version)
-	if res != nil && res.Outdated {
-		msg := fmt.Sprintf("%s is not latest, you should upgrade to v%s", version, res.Current)
+	println(fmt.Sprintf("Version: %s", application.VersionString()))
+	if application.IsOutdatedVersion() {
+		msg := fmt.Sprintf(
+			"%s is not latest, you should upgrade to v%s",
+			application.VersionStringShort(),
+			application.CurrentVersion(),
+		)
 		println(msg)
-		os.Exit(0)
-		return
 	}
+
+	os.Exit(0)
 }
---
+++ application/version.go
@@ -0,0 +1,38 @@
+package application
+
+import (
+	"fmt"
+	"github.com/tcnksm/go-latest"
+)
+
+var (
+	version  = "dev"
+	revision = "unknown"
+	checked  = &latest.CheckResponse{Outdated: false, Current: "dev"}
+)
+
+func init() {
+	if res, err := latest.Check(&latest.GithubTag{Owner: "duck8823", Repository: "duci"}, version); err == nil {
+		checked = res
+	}
+}
+
+// VersionString returns application version with revision (commit hash)
+func VersionString() string {
+	return fmt.Sprintf("%s (%s)", version, revision)
+}
+
+// VersionStringShort returns application version
+func VersionStringShort() string {
+	return version
+}
+
+// IsOutdatedVersion return witch outdated version or not
+func IsOutdatedVersion() bool {
+	return checked.Outdated
+}
+
+// CurrentVersion returns current version string
+func CurrentVersion() string {
+	return checked.Current
+}
