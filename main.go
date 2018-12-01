fb910a17b67f13de865ff6866a1be5d77890c89a
shunsuke maeda <duck8823@gmail.com>
2018-12-01 21:51:24 +0900

    Revert: commit b702f4106b5677c5692e34f56b113720c411e653

--- main.go
+++ main.go
@@ -1,36 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/duck8823/duci/application/service/executor"
-	"github.com/duck8823/duci/domain/model/job"
-	"github.com/duck8823/duci/domain/model/job/target"
-	"github.com/duck8823/duci/domain/model/runner"
+	"github.com/duck8823/duci/application/cmd"
+	"os"
 )
 
 func main() {
-	e := &executor.JobExecutor{
-		DockerRunner: runner.
-			DefaultDockerRunnerBuilder().
-			LogFunc(func(_ context.Context, log job.Log) {
-				for line, err := log.ReadLine(); err == nil; line, err = log.ReadLine() {
-					println(line.Message)
-				}
-			}).
-			Build(),
-		StartFunc: func(_ context.Context) {
-			println("Job Started")
-		},
-		EndFunc: func(_ context.Context, err error) {
-			if err != nil {
-				println(fmt.Sprintf("%+v", err))
-			}
-			println("Job End")
-		},
-	}
-
-	if err := e.Execute(context.Background(), &target.Local{Path: "."}); err != nil {
-		println(fmt.Sprintf("%+v", err))
-	}
+	cmd.Execute(os.Args[1:])
 }
