bfe1e8f7ca6d9139abdba17455d413d277cfbf66
shunsuke maeda <duck8823@gmail.com>
2018-12-02 22:12:10 +0900

    Change to use model

--- application/cmd/health.go
+++ application/cmd/health.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
-	"github.com/duck8823/duci/application/service/docker"
-	"github.com/duck8823/duci/infrastructure/logger"
-	"github.com/google/uuid"
+	"fmt"
+	"github.com/duck8823/duci/domain/model/docker"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -13,17 +12,20 @@ var healthCmd = createCmd("health", "Health check", healthCheck)
 func healthCheck(cmd *cobra.Command, _ []string) {
 	readConfiguration(cmd)
 
-	dockerService, err := docker.New()
+	cli, err := docker.New()
 	if err != nil {
-		logger.Errorf(uuid.New(), "Failed to set configuration.\n%+v", err)
+		msg := fmt.Sprintf("Failed to set configuration.\n%+v", err)
+		if _, err := fmt.Fprint(os.Stderr, msg); err != nil {
+			println(err)
+		}
 		os.Exit(1)
 	}
 
-	if err := dockerService.Status(); err != nil {
-		logger.Errorf(uuid.New(), "Unhealthy.\n%s", err)
+	if err := cli.Status(); err != nil {
+		println(fmt.Sprintf("Unhealth\n%s", err))
 		os.Exit(1)
 	} else {
-		logger.Info(uuid.New(), "ok.")
+		println("ok")
 		os.Exit(0)
 	}
 }
