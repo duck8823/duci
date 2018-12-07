9ade4dc1f76c6dd867167a7cbd97f4ba5797dc39
shunsuke maeda <duck8823@gmail.com>
2018-12-08 00:52:55 +0900

    Using new api

--- presentation/router/router.go
+++ presentation/router/router.go
@@ -2,48 +2,29 @@ package router
 
 import (
 	"context"
+	"fmt"
 	"github.com/duck8823/duci/application"
-	"github.com/duck8823/duci/application/service/docker"
 	"github.com/duck8823/duci/application/service/executor"
-	"github.com/duck8823/duci/application/service/git"
-	"github.com/duck8823/duci/application/service/github"
-	"github.com/duck8823/duci/application/service/logstore"
+	"github.com/duck8823/duci/application/service/job"
 	"github.com/duck8823/duci/application/service/runner"
+	"github.com/duck8823/duci/domain/model/docker"
 	"github.com/duck8823/duci/domain/model/job"
-	git2 "github.com/duck8823/duci/domain/model/job/target/git"
-	github2 "github.com/duck8823/duci/domain/model/job/target/github"
-	"github.com/duck8823/duci/presentation/controller"
-	webhook2 "github.com/duck8823/duci/presentation/controller/webhook"
+	"github.com/duck8823/duci/domain/model/job/target/git"
+	"github.com/duck8823/duci/domain/model/job/target/github"
+	health_controller "github.com/duck8823/duci/presentation/controller/health"
+	job_controller "github.com/duck8823/duci/presentation/controller/job"
+	webhook_controller "github.com/duck8823/duci/presentation/controller/webhook"
 	"github.com/go-chi/chi"
 	"github.com/pkg/errors"
 	"net/http"
 	"os"
+	"time"
 )
 
 // New returns handler of application.
 func New() (http.Handler, error) {
-	dockerService, logstoreService, githubService, err := createCommonServices()
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	dockerRunner, err := createRunner(logstoreService, githubService, dockerService)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	webhooksCtrl := &controller.WebhooksController{Runner: dockerRunner, GitHub: githubService}
-	logCtrl := &controller.LogController{LogStore: logstoreService}
-	healthCtrl := &controller.HealthController{Docker: dockerService}
-
-	rtr := chi.NewRouter()
-	rtr.Post("/", webhooksCtrl.ServeHTTP)
-	rtr.Get("/logs/{uuid}", logCtrl.ServeHTTP)
-	rtr.Get("/health", healthCtrl.ServeHTTP)
-
-	// using new api
 	// FIXME: where initialize??
-	if err := git2.InitializeWithHTTP(func(ctx context.Context, log job.Log) {
+	if err := git.InitializeWithHTTP(func(ctx context.Context, log job.Log) {
 		for line, err := log.ReadLine(); err == nil; line, err = log.ReadLine() {
 			println(line.Message)
 		}
@@ -51,56 +32,112 @@ func New() (http.Handler, error) {
 		return nil, errors.WithStack(err)
 	}
 
-	if err := github2.Initialize(os.Getenv("GITHUB_TOKEN")); err != nil {
+	if err := github.Initialize(os.Getenv("GITHUB_TOKEN")); err != nil {
 		return nil, errors.WithStack(err)
 	}
 
-	webhook := &webhook2.Handler{
+	gh, err := github.GetInstance()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	jobService, err := job_service.New()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	webhook := &webhook_controller.Handler{
 		Executor: executor.DefaultExecutorBuilder().
+			StartFunc(func(ctx context.Context) {
+				buildJob, err := application.BuildJobFromContext(ctx)
+				id := job.ID(buildJob.ID)
+				if err != nil {
+					_ = jobService.Append(id, job.LogLine{Timestamp: time.Now(), Message: err.Error()})
+					return
+				}
+				if err := jobService.Start(id); err != nil {
+					_ = jobService.Append(id, job.LogLine{Timestamp: time.Now(), Message: err.Error()})
+					return
+				}
+				_ = gh.CreateCommitStatus(ctx, github.CommitStatus{
+					TargetSource: buildJob.TargetSource,
+					State:        github.PENDING,
+					Description:  "pending",
+					Context:      buildJob.TaskName,
+					TargetURL:    buildJob.TargetURL,
+				})
+			}).
 			LogFunc(func(ctx context.Context, log job.Log) {
+				buildJob, err := application.BuildJobFromContext(ctx)
+				id := job.ID(buildJob.ID)
+				if err != nil {
+					_ = jobService.Append(id, job.LogLine{Timestamp: time.Now(), Message: err.Error()})
+					return
+				}
 				for line, err := log.ReadLine(); err == nil; line, err = log.ReadLine() {
 					println(line.Message)
+					_ = jobService.Append(id, *line)
+				}
+			}).
+			EndFunc(func(ctx context.Context, e error) {
+				buildJob, err := application.BuildJobFromContext(ctx)
+				id := job.ID(buildJob.ID)
+				if err != nil {
+					_ = jobService.Append(id, job.LogLine{Timestamp: time.Now(), Message: err.Error()})
+					return
+				}
+				if err := jobService.Finish(id); err != nil {
+					println(fmt.Sprintf("%+v", err))
+					return
+				}
+
+				switch e {
+				case nil:
+					_ = gh.CreateCommitStatus(ctx, github.CommitStatus{
+						TargetSource: buildJob.TargetSource,
+						State:        github.SUCCESS,
+						Description:  "success",
+						Context:      buildJob.TaskName,
+						TargetURL:    buildJob.TargetURL,
+					})
+				case runner.ErrFailure:
+					_ = gh.CreateCommitStatus(ctx, github.CommitStatus{
+						TargetSource: buildJob.TargetSource,
+						State:        github.FAILURE,
+						Description:  "failure",
+						Context:      buildJob.TaskName,
+						TargetURL:    buildJob.TargetURL,
+					})
+				default:
+					_ = gh.CreateCommitStatus(ctx, github.CommitStatus{
+						TargetSource: buildJob.TargetSource,
+						State:        github.ERROR,
+						Description:  github.Description(fmt.Sprintf("error: %s", e.Error())),
+						Context:      buildJob.TaskName,
+						TargetURL:    buildJob.TargetURL,
+					})
 				}
 			}).
 			Build(),
 	}
 
-	rtr.Post("/webhook", webhook.ServeHTTP)
-
-	return rtr, nil
-}
-
-func createCommonServices() (docker.Service, logstore.Service, github.Service, error) {
-	dockerService, err := docker.New()
-	if err != nil {
-		return nil, nil, nil, errors.WithStack(err)
+	job := &job_controller.Handler{
+		Service: jobService,
 	}
 
-	logstoreService, err := logstore.New()
-	if err != nil {
-		return nil, nil, nil, errors.WithStack(err)
-	}
-	githubService, err := github.New()
-	if err != nil {
-		return nil, nil, nil, errors.WithStack(err)
-	}
-
-	return dockerService, logstoreService, githubService, nil
-}
-
-func createRunner(logstoreService logstore.Service, githubService github.Service, dockerService docker.Service) (runner.Runner, error) {
-	gitClient, err := git.New()
+	docker, err := docker.New()
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
-	dockerRunner := &runner.DockerRunner{
-		BaseWorkDir: application.Config.Server.WorkDir,
-		Git:         gitClient,
-		GitHub:      githubService,
-		Docker:      dockerService,
-		LogStore:    logstoreService,
+	health := &health_controller.Handler{
+		Docker: docker,
 	}
 
-	return dockerRunner, nil
+	rtr := chi.NewRouter()
+	rtr.Post("/", webhook.ServeHTTP)
+	rtr.Get("/logs/{uuid}", job.ServeHTTP)
+	rtr.Get("/health", health.ServeHTTP)
+
+	return rtr, nil
 }
