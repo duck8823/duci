a5b8c817bbb1a82a59dbf4279afdbd795356457d
duck8823 <duck8823@gmail.com>
2019-01-03 11:36:44 +0900

    Add assertion

--- presentation/controller/webhook/export_test.go
+++ presentation/controller/webhook/export_test.go
@@ -2,6 +2,10 @@ package webhook
 
 import (
 	"github.com/duck8823/duci/application/service/executor"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"net/url"
+	"reflect"
 )
 
 type Handler = handler
@@ -12,4 +16,35 @@ func (h *Handler) SetExecutor(executor executor.Executor) (reset func()) {
 	return func() {
 		h.executor = tmp
 	}
+}
+
+func URLMust(url *url.URL, err error) *url.URL {
+	if err != nil {
+		panic(err)
+	}
+	return url
+}
+
+func CmpOptsAllowFields(typ interface{}, names ...string) cmp.Option {
+	return cmpopts.IgnoreFields(typ, func() []string {
+		var ignoreFields []string
+
+		t := reflect.TypeOf(typ)
+		for i := 0; i < t.NumField(); i++ {
+			name := t.Field(i).Name
+			if !contains(names, name) {
+				ignoreFields = append(ignoreFields, name)
+			}
+		}
+		return ignoreFields
+	}()...)
+}
+
+func contains(s []string, e string) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
 }
--- presentation/controller/webhook/handler_test.go
+++ presentation/controller/webhook/handler_test.go
@@ -2,6 +2,7 @@ package webhook_test
 
 import (
 	"context"
+	"github.com/duck8823/duci/application"
 	"github.com/duck8823/duci/application/service/executor/mock_executor"
 	"github.com/duck8823/duci/application/service/job"
 	"github.com/duck8823/duci/domain/model/job"
@@ -9,9 +10,15 @@ import (
 	"github.com/duck8823/duci/internal/container"
 	"github.com/duck8823/duci/presentation/controller/webhook"
 	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+	go_github "github.com/google/go-github/github"
+	"github.com/google/uuid"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -96,7 +103,36 @@ func TestHandler_PushEvent(t *testing.T) {
 		Execute(gomock.Any(), gomock.Any()).
 		Times(1).
 		Do(func(ctx context.Context, target job.Target) {
-			// TODO: check argument
+			got, err := application.BuildJobFromContext(ctx)
+			if err != nil {
+				t.Errorf("must not be nil, but got %+v", err)
+			}
+
+			want := &application.BuildJob{
+				ID: job.ID(uuid.Must(uuid.Parse("72d3162e-cc78-11e3-81ab-4c9367dc0958"))),
+				TargetSource: &github.TargetSource{
+					Repository: &go_github.PushEventRepository{
+						ID:       go_github.Int64(135493233),
+						FullName: go_github.String("Codertocat/Hello-World"),
+						SSHURL:   go_github.String("git@github.com:Codertocat/Hello-World.git"),
+						CloneURL: go_github.String("https://github.com/Codertocat/Hello-World.git"),
+					},
+					Ref: "refs/tags/simple-tag",
+					SHA: plumbing.ZeroHash,
+				},
+				TaskName:  "duci/push",
+				TargetURL: webhook.URLMust(url.Parse("http://example.com/")),
+			}
+
+			opt := webhook.CmpOptsAllowFields(go_github.PushEventRepository{}, "ID", "FullName", "SSHURL", "CloneURL")
+			if !cmp.Equal(got, want, opt) {
+				t.Errorf("must be equal but: %+v", cmp.Diff(got, want, opt))
+			}
+
+			typ := reflect.TypeOf(target).String()
+			if typ != "*target.GitHub" {
+				t.Errorf("type must be *target.GitHub, but got %s", typ)
+			}
 		}).
 		Return(nil)
 
